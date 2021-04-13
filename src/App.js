8c8c33a3231b1f2d199de0843fb86a49ba2c4f91
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2021-04-13 12:04:18 +0200

    e

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import me from "./images/TOM.png";
+import Banner from "./components/Banner";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div  id="whatever" className="App">
+
+      <Banner className="header" />
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
+{
+  /* import { FaBeer } from 'react-icons/fa';
+class Question extends React.Component {
+  render() {
+    return <h3> Lets go for a <FaBeer />? </h3>
+  }
+} */
+}
+{
+  /* <img className="headerimg" src={me} alt="mypicture" /> */
+}
---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <div className="header__content"></div>
+          </div>
+          <div className="col-6"></div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Banner;
